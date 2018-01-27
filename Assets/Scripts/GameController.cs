43eb78d20adff3d4cc9ddfae87b8d1cf8f728d45
Henry <hanokh.aloni@gmail.com>
2018-01-27 13:03:46 +0200

    Added timer for gameover sign. Added gamover sign.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,18 +8,54 @@ public class GameController : MonoBehaviour {
     public int PlayerOneScore = 0;
     public int PlayerTwoScore = 0;
 
+    public float timeLeft = 60.0f;
+
     public Text PlayerOneScoreText;
     public Text PlayerTwoScoreText;
+    public Text TimeRemainingText;
+
+
+
+
+    public Text GameOverText;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    }
+
+
+    public void UpdateTime()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            GameOver();
+        }
+
+        if (timeLeft > 0.0f)
+        {
+            TimeRemainingText.text = string.Format("{0:#,##} Seconds", timeLeft);
+
+        }
+        else
+        {
+            TimeRemainingText.text = "OUT OF TIME!";
+        }
+
+    }
+
+    public void GameOver()
+    {
+        GameOverText.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        UpdateTime();
+
+
+    }
 
     public void IncrementPlayerScore(int playerNumber)
     {
